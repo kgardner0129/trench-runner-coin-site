81f42c75d2aab98a2a48e0216826e3e09a659ac8
kgardner0129 <kgardner0129@gmail.com>
2026-06-30 10:25:34 -0500

    Add DEX fund wallet

--- coin_config.js
+++ coin_config.js
@@ -2,11 +2,12 @@ window.TrenchCoinConfig = {
   // After creating the Pump.fun coin, paste the token mint address here.
   mint: "9m7AQ8LUHi8gbrtjh8fdcyoTLFgD4AzGQh2mZiTFpump",
   minHoldTokens: 100,
+  requireHolderToPlay: false,
   rpcUrl: "https://api.mainnet-beta.solana.com",
   // After creating the Pump.fun coin, paste the direct coin page URL here.
   purchaseUrl: "https://pump.fun/coin/9m7AQ8LUHi8gbrtjh8fdcyoTLFgD4AzGQh2mZiTFpump",
   // Treasury wallet that receives the SOL DEX-fund contribution.
-  dexFundWallet: "",
+  dexFundWallet: "2v6ZGBSLEiA8YA4nfkMbtudybNSeUafhjGttg4EK3JR8",
   dexContributionUsd: 1,
   dexGoalUsd: 299,
   // After deploying the settlement API, paste its HTTPS URL here.
